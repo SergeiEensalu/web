c0f0ce07b9c0bdf067284286d57ff0924110811d
Sergei Eensalu <sergei.eensalu@gmail.com>
2024-06-17 21:38:25 +0300

    Update url

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'http://localhost:3000'; // Replace with your backend URL
-export const BASE_URL = 'https://dffb-82-131-110-53.ngrok-free.app'; // TODO: REPLACE THIS!
+export const BASE_URL = `${YOUR_URL}`; // TODO: REPLACE 'YOUR_URL' WITH YOU LINK 2.
 
 export interface ApiResponse {
   ok: boolean;
